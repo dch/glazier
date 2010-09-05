388a8c278031ced69530561e68deaa479b1f88ae
dch <dave@muse.net.nz>
2010-09-05 13:58:40 +1200

    reglaze fetches latest bundle from github

--- bin/get_bits.cmd
+++ bin/get_bits.cmd
@@ -6,7 +6,7 @@ echo START	fetching %1
 if  exist "curl_%1.txt" goto single_file
 
 :: check for multiple files
-for %%f in (curl_*.txt) do @echo processing [%%f] & for /f "eol=# tokens=1,2,3,4" %%i in (%%f) do @echo [%%i] & if not exist %%j echo fetching from %%k & curl.exe -L %%k --output %%j
+for %%f in (curl_*.txt) do @echo processing [%%f] & for /f "eol=# tokens=1,2,3,4" %%i in (%%f) do @echo [%%i] & if not exist %%j echo fetching from %%k & curl.exe -#L %%k --output %%j
 
 :: now exit
 goto eof
@@ -19,7 +19,7 @@ goto eof
 :: %l md5 hash check
 :: TODO implement md5 check
 :: if the filename doesn't exist, fetch it from the URL
-for /f "eol=# tokens=1,2,3,4" %%i in (curl_%1.txt) do @echo [%%i] & if not exist %%j echo fetching from %%k & curl.exe -L %%k --output %%j
+for /f "eol=# tokens=1,2,3,4" %%i in (curl_%1.txt) do @echo [%%i] & if not exist %%j echo fetching from %%k & curl.exe -#L %%k --output %%j
 
 :eof
 echo DONE	fetching %1
---
+++ bin/reglaze.cmd
@@ -0,0 +1,1 @@
+curl -#L http://github.com/dch/glazier/zipball/master --output glazier_current.zip

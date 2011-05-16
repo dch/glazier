cbe3aa82d7129c3b156903434becb6427780989f
Dave Cottlehuber <dave@muse.net.nz>
2011-05-17 00:38:43 +1200

    bump to curl 7.21.5 and newer spidermonkey location

--- bin/couchdb_config.sh
+++ bin/couchdb_config.sh
@@ -6,11 +6,11 @@ export COUCH_TOP
 --prefix=$ERL_TOP/release/win32 \
 --with-erlang=$ERL_TOP/release/win32/usr/include \
 --with-win32-icu-binaries=/relax/icu \
---with-win32-curl=/relax/curl-7.21.3 \
+--with-win32-curl=/relax/curl-7.21.5 \
 --with-openssl-bin-dir=/relax/openssl/bin \
 --with-msvc-redist-dir=/relax \
---with-js-lib=/relax/spidermonkey/js/src/dist/lib \
---with-js-include=/relax/spidermonkey/js/src/dist/include \
+--with-js-lib=/relax/tracemonkey-57a6ad20eae9/js/src/dist/lib \
+--with-js-include=/relax/tracemonkey-57a6ad20eae9/js/src/dist/include \
 2>&1 | tee $COUCH_TOP/build_configure.txt
 
 echo DONE. | tee -a $COUCH_TOP/build_configure.txt

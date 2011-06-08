c4251ac5cf3482169625b789529d644b88ff9bbb
Dave Cottlehuber <dave@muse.net.nz>
2011-06-09 07:23:38 +1200

    revert erlang support for dynamic usage of SSL - static is our friend

--- bin/erl_config.sh
+++ bin/erl_config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ./otp_build autoconf 2>&1 | tee $ERL_TOP/build_autoconf.txt
+# ./otp_build configure --enable-dynamic-ssl-lib 2>&1 | tee $ERL_TOP/build_configure.txt
 ./otp_build configure 2>&1 | tee $ERL_TOP/build_configure.txt
-

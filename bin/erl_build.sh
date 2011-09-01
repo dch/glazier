f6caa75e51b77b77a01607eac58878fd9850fe68
Dave Cottlehuber <dave@muse.net.nz>
2011-09-01 14:37:02 +1200

    Force dynamic binding of OpenSSL
    Broken in R14B03
    Incomplete patch provided upstream

--- bin/erl_build.sh
+++ bin/erl_build.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
+pushd $ERL_TOP
 ./otp_build boot -a  2>&1 | tee $ERL_TOP/build_boot.txt
 echo DONE. | tee -a $ERL_TOP/build_boot.txt
 
 ./otp_build release -a  2>&1 | tee $ERL_TOP/build_release.txt
 echo DONE. | tee -a $ERL_TOP/build_release.txt
 
-# needed to get the same vcredist build as SxW manifests into installer .exe
-## cp $ERL_TOP/../vcredist_x86.exe $ERL_TOP/release/win32/
-
 ./otp_build installer_win32  2>&1 | tee $ERL_TOP/build_installer_win32.txt
 mv --force $ERL_TOP/release/win32/otp_win32_R*.exe /relax/release/  2>&1 | tee -a $ERL_TOP/build_installer_win32.txt
 
 ./release/win32/Install.exe -s 2>&1 | tee -a $ERL_TOP/build_release.txt
 echo DONE. | tee -a $ERL_TOP/build_release.txt 
-
+popd
--- bin/erl_config.sh
+++ bin/erl_config.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+pushd $ERL_TOP
 ./otp_build autoconf 2>&1 | tee $ERL_TOP/build_autoconf.txt
-# ./otp_build configure --enable-dynamic-ssl-lib 2>&1 | tee $ERL_TOP/build_configure.txt
-./otp_build configure 2>&1 | tee $ERL_TOP/build_configure.txt
+# from Erlang/OTP R14B03 onwards, Erlang by default is built with
+# *static* OpenSSL. Uncomment following, and change build_openssl.cmd
+# to revert this.
+./otp_build configure --enable-dynamic-ssl-lib 2>&1 | tee $ERL_TOP/build_configure.txt
+## ./otp_build configure --with-ssl=/relax/openssl 2>&1 | tee $ERL_TOP/build_configure.txt
+popd

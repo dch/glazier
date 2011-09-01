030fca33000eac6da0da54dbb5fe4322e52a5cb7
Dave Cottlehuber <dave@muse.net.nz>
2011-09-01 17:31:45 +1200

    add wxWidget docs and prep for Windows SDK 7.0

--- bin/build_wx.cmd
+++ bin/build_wx.cmd
@@ -1,20 +1,25 @@
 setlocal
-call "%vs90comntools%\..\..\vc\vcvarsall.bat" x86
-path=%path%;%relax%\7zip;%glazier%\bin;%glazier%\bits;
+path=%path%;%relax%\7zip;
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+if exist c:\cygwin\opt\local\pgm\wxWidgets-2.8.11 goto build
+if exist %relax%\wxMSW-2.8.11 goto build
+start /wait %RELAX%\7zip\7z.exe x %RELAX%\bits\wxMSW* -aoa -y -o%RELAX%\
+mkdir c:\cygwin\opt\local\pgm > NUL: 2>&1
+mklink /d c:\cygwin\opt\local\pgm\wxWidgets-2.8.11 c:\relax\wxMSW-2.8.11
 
-
-pushd %RELAX%\wxMSW*\build\msw
-set CL=/D_BIND_TO_CURRENT_VCLIBS_VERSION=1
-vcbuild /useenv  /platform:Win32 /M4 wx.sln "Unicode Release|Win32"
-vcbuild /useenv  /platform:Win32 /M4 wx.sln "Unicode Debug|Win32"
-popd
-
-
-pushd %RELAX%\wxMSW*\contrib\build\stc
-vcbuild /useenv /platform:Win32 /M4 stc.sln "Unicode Release|Win32"
-vcbuild /useenv /platform:Win32 /M4 stc.sln "Unicode Debug|Win32"
+:build
+pushd %RELAX%\wxMSW*\build\msw
+set CL=/D_BIND_TO_CURRENT_VCLIBS_VERSION=1
+vcbuild /useenv  /platform:Win32 /M4 wx.sln "Unicode Release|Win32"
+vcbuild /useenv  /platform:Win32 /M4 wx.sln "Unicode Debug|Win32"
 popd
-
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+pushd %RELAX%\wxMSW*\contrib\build\stc
+vcbuild /useenv /platform:Win32 /M4 stc.sln "Unicode Release|Win32"
+vcbuild /useenv /platform:Win32 /M4 stc.sln "Unicode Debug|Win32"
+popd
 endlocal
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::

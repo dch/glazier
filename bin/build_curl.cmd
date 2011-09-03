044bfd0e04cac2077ea549ff1a3faef519b42079
Dave Cottlehuber <dave@muse.net.nz>
2011-09-04 00:55:08 +1200

    automate building of libcurl and update docs

--- bin/build_curl.cmd
+++ bin/build_curl.cmd
@@ -1,35 +1,40 @@
 ::setlocal
-path=%path%;%relax%\7zip;
+path=%path%;%relax%\cmake\bin;%relax%\7zip;%relax%\openssl\bin;
 
 for %%i in ("%RELAX%\bits\curl-*.zip") do set curl_ver=%%~ni
-set CURL_PATH=%RELAX%\%curl_ver%
+setx CURL_VER %curl_ver%
+set CURL_SRC=%RELAX%\%curl_ver%
+setx CURL_SRC %curl_src%
+set CURL_PATH=%relax%\curl
+setx CURL_PATH %curl_path%
 
-if not defined SSL_PATH echo OpenSSL not found && goto eof
-::set USE_SSLEAY=1
-::set USE_OPENSSL=1
+:: settings for CMake
+set install=%curl_path%
 
-:: set path for curl & couch compilation later on
-set INCLUDE=%INCLUDE%;%SSL_PATH%\include\openssl;%SSL_PATH%\include;%CURL_PATH%\include\curl;
-set LIBPATH=%LIBPATH%;%SSL_PATH%\lib;%CURL_PATH%\lib;
-set LIB=%LIB%;%SSL_PATH%\lib;%CURL_PATH%\lib;
-
-:: set LINK & CL to resolve manifest binding issues & virtualisation hack in ld.sh#171
-set CL=/D_BIND_TO_CURRENT_VCLIBS_VERSION=1
+if not defined SSL_PATH echo OpenSSL not built && goto eof
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: clean up existing installs
 :: extract bundle and name
 :: stash SSL version
-if defined curl_ver rd /s/q %relax%\%curl_ver%
-setx curl_ver %curl_ver%
+if exist %curl_path% rd /s/q %curl_path%
+if defined curl_ver rd /s/q %curl_src%
 7z x "%RELAX%\bits\curl-*.zip" -o%RELAX% -y
-popd
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-pushd %relax%\%curl_ver%\winbuild
-nmake /f makefile.vc mode=static use_sspi=no with_ssl=static
-:: TODO do we *need* zlib?
-:: TODO these files should be put into $RELAX/curl/
-copy ..\builds\libcurl-release-static-ssl-dll-ipv6\bin\curl.exe %glazier%\bits;
+pushd %relax%\curl-7*
+
+:: settings for Compiler
+set USE_SSLEAY=1
+set USE_OPENSSL=1
+set INCLUDE=%INCLUDE%;%SSL_PATH%\include;%SSL_PATH%\include\openssl;
+set LIBPATH=%LIBPATH%;%SSL_PATH%\lib;
+set LIB=%LIB%;%SSL_PATH%\lib;
+set CL=/D_BIND_TO_CURRENT_VCLIBS_VERSION=1
+
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D BUILD_CURL_TESTS=No -D CURL_STATICLIB=Yes -D CURL_ZLIB=No  -D CMAKE_INSTALL_PREFIX="%curl_path%" -H"%curl_src%" -B"%temp%\%curl_ver%"
+cmake --build "%temp%\%curl_ver%" --target install
+popd
+dir /b %install%\bin %install%\lib
 
 popd
 endlocal
